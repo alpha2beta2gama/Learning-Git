bd95a8a13adb43471e7683b21608619a04f99ff1
alpha2beta2gama <56175770+alpha2beta2gama@users.noreply.github.com>
2019-10-04 16:30:15 -0500

    Dog and cat

--- test.java
+++ test.java
@@ -3,7 +3,13 @@ package OOPsConceptPart2;
 public class TestAnimal {
 
 	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.barks();
+		d.eat();
 		
+		Cat c = new Cat();
+		c.eat();
+		c.meows();
 		
 		Animal a = new Animal();
 		a.eat();
